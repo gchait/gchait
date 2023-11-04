8495f16f6346a1544e9319eaf3e57c2bfa154fd8
Guy Chait <53366531+gchait@users.noreply.github.com>
2023-11-04 17:48:15 +0200

    Update scoop-pkgs.ps1

--- WinHost/scoop-pkgs.ps1
+++ WinHost/scoop-pkgs.ps1
@@ -4,8 +4,15 @@ scoop bucket add nerd-fonts
 scoop install 7zip
 scoop install wezterm
 scoop install vscodium
+
 reg import "$HOME\scoop\apps\vscodium\current\install-context.reg"
 reg import "$HOME\scoop\apps\vscodium\current\install-associations.reg"
+
+codium --install-extension Catppuccin.catppuccin-vsc-icons
+codium --install-extension jeanp413.open-remote-ssh
+codium --install-extension khan.two-monokai
+codium --install-extension olifink.fedora-gnome-light-dark
+codium --install-extension PKief.material-product-icons
 
 scoop install grep
 scoop install vagrant
