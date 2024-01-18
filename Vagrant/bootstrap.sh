64f407a0ec57995eefc34245616fedcd68f54b62
Guy Chait <53366531+gchait@users.noreply.github.com>
2024-01-18 16:02:34 +0200

    Update bootstrap.sh

--- Vagrant/bootstrap.sh
+++ Vagrant/bootstrap.sh
@@ -1,15 +1,21 @@
 #!/bin/bash -xe
 
+export OS_USERNAME="vagrant"
+
 configure_user() {
     # NvChad
     [[ -d ~/.config/nvim ]] || git clone \
         https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
 
     # Dotfiles
-    cp -r /vagrant/.config ~/
+    if [ ${OS_USERNAME} = "vagrant" ]; then
+        cp -r /vagrant/.config ~/
+    else
+        cp -r ./Home/.* ~/
+    fi
 
     # System-level Python packages
-    pip3 install isort black flake8 bandit requests pyyaml neovim
+    pip3 install isort ruff bandit requests pyyaml neovim
 
     # Git pre-setup
     [[ -f ~/.ssh/id_rsa.pub ]] || ssh-keygen -f ~/.ssh/id_rsa -q -N ""
@@ -19,10 +25,10 @@ configure_user() {
 # Base repo packages
 dnf update -y
 dnf install --setopt=install_weak_deps=false -y \
-    tree sl vim neovim make ripgrep dnf-plugins-core util-linux-user \
-    python3-setuptools python3-pip cmatrix neofetch wget awscli2 openssl \
-    zip gzip tar jq cloud-utils fish git lolcat python3-wheel exa \
-    ca-certificates gnupg gcc curl dnsutils python3-netaddr npm htop
+    tree sl vim neovim make ripgrep dnf-plugins-core util-linux-user figlet zsh asciiquarium \
+    python3-setuptools python3-pip cmatrix neofetch wget awscli2 openssl asciinema \
+    zip gzip tar jq cloud-utils zsh-autosuggestions git lolcat python3-wheel eza poetry \
+    ca-certificates gnupg gcc curl dnsutils python3-netaddr npm htop just postgresql
 
 # Docker
 dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
@@ -32,19 +38,14 @@ systemctl restart docker
 systemctl enable docker
 usermod -aG docker vagrant
 
-# Amazon Corretto
-rpm --import https://yum.corretto.aws/corretto.key 
-curl -sLo /etc/yum.repos.d/corretto.repo https://yum.corretto.aws/corretto.repo
-dnf install -y java-17-amazon-corretto-devel
-
 # Kubectl
 cat <<EOF | tee /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
-baseurl=https://pkgs.k8s.io/core:/stable:/v1.27/rpm/
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/
 enabled=1
 gpgcheck=1
-gpgkey=https://pkgs.k8s.io/core:/stable:/v1.27/rpm/repodata/repomd.xml.key
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/repodata/repomd.xml.key
 EOF
 dnf install -y kubectl
 
@@ -64,26 +65,20 @@ wget -qO /usr/local/bin/yq \
     https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64
 chmod +x /usr/local/bin/yq
 
-wget -qO /usr/local/bin/websocat \
-    https://github.com/vi/websocat/releases/latest/download/websocat.x86_64-unknown-linux-musl
-chmod +x /usr/local/bin/websocat
+if [ ${OS_USERNAME} = "vagrant" ]; then
+    # De-bloat
+    hostnamectl hostname fedora
+    > /etc/motd
+    grep "#PrintLastLog yes" /etc/ssh/sshd_config \
+        && sed -i 's/#PrintLastLog yes/PrintLastLog no/' /etc/ssh/sshd_config \
+        && systemctl restart sshd
+    
+    # Ensure the filesystem takes all the space
+    growpart /dev/sda 2 || true
+    xfs_growfs /dev/sda2 || true
+fi
 
-[[ -f /usr/local/bin/just ]] || curl --proto '=https' \
-    --tlsv1.2 -sSf https://just.systems/install.sh | \
-    bash -s -- --to /usr/local/bin/
-
-# De-bloat
-hostnamectl hostname fedora
-> /etc/motd
-grep "#PrintLastLog yes" /etc/ssh/sshd_config \
-    && sed -i 's/#PrintLastLog yes/PrintLastLog no/' /etc/ssh/sshd_config \
-    && systemctl restart sshd
-
-# Ensure the filesystem takes all the space
-growpart /dev/sda 2 || true
-xfs_growfs /dev/sda2 || true
-
-# User configuration for vagrant
+# Regular user configuration
 export -f configure_user
-su vagrant -c "bash -xec configure_user"
-chsh -s $(which fish) vagrant
+su ${OS_USERNAME} -c "bash -xec configure_user"
+chsh -s $(which fish) ${OS_USERNAME}
