74a413349f011de654742f93cbe01fe68b7ee279
Guy Chait <>
2024-01-21 10:32:34 +0200

    pretty

--- LinHost/codium.sh
+++ LinHost/codium.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
+# codium --list-extensions | awk '{print "codium --install-extension " $0}'
 
+codium --install-extension bungcip.better-toml
 codium --install-extension Catppuccin.catppuccin-vsc-icons
-codium --install-extension efoerster.texlab
-codium --install-extension khan.two-monokai
-codium --install-extension PKief.material-product-icons
 codium --install-extension eamodio.gitlens
 codium --install-extension ecmel.vscode-html-css
+codium --install-extension efoerster.texlab
 codium --install-extension esbenp.prettier-vscode
-codium --install-extension ms-pyright.pyright
-codium --install-extension redhat.vscode-yaml
 codium --install-extension kokakiwi.vscode-just
+codium --install-extension ms-pyright.pyright
+codium --install-extension PKief.material-product-icons
+codium --install-extension pr1sm8.theme-panda
+codium --install-extension redhat.vscode-yaml
 codium --install-extension samuelcolvin.jinjahtml
-codium --install-extension bungcip.better-toml
