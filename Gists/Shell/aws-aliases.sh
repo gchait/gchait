fe447018c96afe4e7a9b9db26a7e01f157e7bbaf
Guy Chait <53366531+gchait@users.noreply.github.com>
2024-03-18 17:13:45 +0200

    Update and rename aws-profile-alias.sh to aws-aliases.sh

--- Gists/Shell/aws-aliases.sh
+++ Gists/Shell/aws-aliases.sh
@@ -1,2 +1,2 @@
 alias awsp='f(){ if [ -z "$1" ]; then; echo $AWS_PROFILE; else; export AWS_PROFILE="$1"; fi; unset -f f; }; f'
-awsp dev
+alias sso="aws sso login 2>&1 | grep -Eo 'https://.+user_code[^:]+'"
