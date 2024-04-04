ed5b4ea56d9a5675915728922e2bfd83699550e1
Guy Chait <53366531+gchait@users.noreply.github.com>
2024-04-04 23:06:55 +0300

    minimalism

--- Linux/Home/.wezterm.lua
+++ Linux/Home/.wezterm.lua
@@ -1,19 +1,7 @@
 local wezterm = require "wezterm"
-local config = {}
-local wallpapers = wezterm.glob(wezterm.home_dir .. "/gchait/Wallpapers/*")
-
-if wezterm.config_builder then
-  config = wezterm.config_builder()
-end
+local config = wezterm.config_builder()
 
 config.color_scheme = "Panda (Gogh)"
 config.font = wezterm.font "Hack Nerd Font Mono"
-
-config.background = {
-  {
-    hsb = { brightness = 0.01 },
-    source = { File = wallpapers[math.random(#wallpapers)] }
-  }
-}
 
 return config
