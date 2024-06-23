2827e7547e4c8f31e13502b8803f9d27b37da4e9
Guy Chait <53366531+gchait@users.noreply.github.com>
2024-06-23 20:44:56 +0300

    clean ps

--- Setup/bootstrap.ps1
+++ Setup/bootstrap.ps1
@@ -1,31 +1,34 @@
 scoop update -a
-scoop bucket add extras
+scoop bucket add extras *> ${null}
 
 scoop install `
     eza fastfetch vim grep spotify jq yq vscode `
     rufus just windows-terminal draw.io gron vlc `
     ccleaner kubectl helm wget zip ripgrep
 
-reg import "${HOME}\scoop\apps\vscode\current\install-context.reg"
-reg import "${HOME}\scoop\apps\vscode\current\install-associations.reg"
+reg import "${HOME}\scoop\apps\vscode\current\install-context.reg" *> ${null}
+reg import "${HOME}\scoop\apps\vscode\current\install-associations.reg" *> ${null}
 
-code --install-extension catppuccin.catppuccin-vsc-icons
-code --install-extension eamodio.gitlens
-code --install-extension ecmel.vscode-html-css
-code --install-extension esbenp.prettier-vscode
-code --install-extension hashicorp.terraform
-code --install-extension kokakiwi.vscode-just
-code --install-extension ms-azuretools.vscode-docker
-code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
-code --install-extension ms-pyright.pyright
-code --install-extension pkief.material-product-icons
-code --install-extension redhat.vscode-yaml
-code --install-extension samuelcolvin.jinjahtml
-code --install-extension tamasfe.even-better-toml
-code --install-extension tinkertrain.theme-panda
+code `
+    --install-extension catppuccin.catppuccin-vsc-icons `
+    --install-extension eamodio.gitlens `
+    --install-extension ecmel.vscode-html-css `
+    --install-extension esbenp.prettier-vscode `
+    --install-extension hashicorp.terraform `
+    --install-extension kokakiwi.vscode-just `
+    --install-extension ms-azuretools.vscode-docker `
+    --install-extension ms-kubernetes-tools.vscode-kubernetes-tools `
+    --install-extension ms-pyright.pyright `
+    --install-extension pkief.material-product-icons `
+    --install-extension redhat.vscode-yaml `
+    --install-extension samuelcolvin.jinjahtml `
+    --install-extension tamasfe.even-better-toml `
+    --install-extension tinkertrain.theme-panda `
+    | Select-String -NotMatch -Pattern "already installed" | % { $_.Line }
 
 Copy-Item -Recurse -Force `
-    -Path "${HOME}\gchait\Setup\Home\*" -Destination "${HOME}"
+    -Path "${HOME}\gchait\Setup\Home\*" `
+    -Destination "${HOME}"
 
 Start-Process -Wait -NoNewWindow `
     -FilePath "${HOME}\scoop\apps\git\current\usr\bin\bash.exe" `
